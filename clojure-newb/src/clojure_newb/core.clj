3604513e303da15eaf880611fd2d7412bccc5282
Philip James <philip@eventbrite.com>
2015-01-03 20:05:48 -0800

    Initial commit

---
+++ clojure-newb/src/clojure_newb/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-newb.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ clojure-newb/test/clojure_newb/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-newb.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-newb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
